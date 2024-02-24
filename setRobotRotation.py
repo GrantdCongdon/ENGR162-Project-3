a476e800fd704e908f9817c0fbfe64f33e59c16b
GrantdCongdon <grantdcongdon@gmail.com>
2024-02-23 19:14:24 -0500

    Improved programs

--- setRobotRotation.py
+++ setRobotRotation.py
@@ -1,7 +1,4 @@
-#import necessary libraries
-import grovepi as gp
 import brickpi3
-from MPU9250 import MPU9250
 from time import sleep
 
 # configure brickpi
@@ -12,21 +9,6 @@ bp.reset_all()
 rightMotor = bp.PORT_A
 leftMotor = bp.PORT_D
 
-motorSpeed = -200
-
-# motor speed
-rightMotorSpeed = 0
-leftMotorSpeed = 0
-
-# distance sensors
-distanceSensor1 = 8
-distanceSensor2 = 6
-distanceSensor3 = 4
-
-# drives the robot forward
-def driveForward(motorSpeed):
-	bp.set_motor_dps(rightMotor+leftMotor, motorSpeed)
-
 # turns the robot
 def turnRobot(rightMotorSpeed, leftMotorSpeed):
 	bp.set_motor_dps(rightMotor, rightMotorSpeed)
@@ -35,44 +17,7 @@ def turnRobot(rightMotorSpeed, leftMotorSpeed):
 # turns off power to the motors
 def stopMotors():
 	bp.set_motor_power(rightMotor+leftMotor, 0)
-
-# get turn differential
-def getTurnDifferential():
-	dist1 = gp.ultrasonicRead(distanceSensor1)
-	dist2 = gp.ultrasonicRead(distanceSensor2)
-	dist3 = gp.ultrasonicRead(distanceSensor3)
-	return [dist1, dist2, dist3]
-
-def followWall():
-	# dist[0] is the rear sensor and dist[1] is the upper sensor
-	while True:
-		try:
-			dists = getTurnDifferential()
-			print(f"Rear sensor: {dists[0]}\tUpper sensor: {dists[1]}\tFront sensor: {dists[2]}")
-			if (dists[1]<=10):
-				turnRobot(0, -100)
-			elif dists[0]<dists[1]-1:
-				turnRobot(-100, 0)
-				print("Turning right")
-			elif dists[1]<dists[0]-1:
-				turnRobot(0, -100)
-				print("Turning left")
-			else:
-				driveForward(motorSpeed)
-				print("Going straight")
-			
-			if (dists[2] < 15):
-				if (dists[1]<15):
-					turn(90, 4)
-				else:
-					turn(-90, 4)
-
-		except KeyboardInterrupt:
-			stopMotors()
-			sleep(0.1)
-			bp.reset_all()
-			break
-
+	
 def turn(degrees, gain):
 	bp.reset_all()
 	gyro = bp.set_sensor_type(bp.PORT_3, bp.SENSOR_TYPE.EV3_GYRO_ABS_DPS)
@@ -123,5 +68,10 @@ def turn(degrees, gain):
 	stopMotors()
 	sleep(0.1)
 
+def main():
+	degrees = float(input("Enter number of degrees to rotate (positive for clockwise): "))
+	turn(degrees, 4)
+
 if __name__ == "__main__":
-    followWall()
+	main()
+	
